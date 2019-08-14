1e3bfa56294df06655f0993a9dcf105328e82a4b
FOLUWA <moronfoluwaakintola@gmail.com>
2019-08-14 02:11:52 +0100

    The initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+// LOAD ENV
+dotenv.config({
+	path: './config.env'
+});
+
+const app = express();
+
+//Dev Logging
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
+
+// Profile Routes
+app.use('/api/v1/profile', require('./routes/profile'));
+
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static(__dirname + '/public/'));
+
+	// Handle SPA
+	app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+	console.log('Server running at ' + process.env.NODE_ENV + ' ' + 'mode on port ' + port);
+});
